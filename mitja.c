fea989c5cf4e3b235edd4299a46ac4801a89feac
frgomo <frgomo1718daw2vib@gmail.com>
2018-05-08 00:02:39 +0200

    Segon commit del projecte mitja

--- mitja.c
+++ mitja.c
@@ -12,7 +12,7 @@ int main() {
 		scanf("%f",&nou_valor);
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
-		printf("Vols continuar (s/n): ");
+		printf("Volss continuar (s/n): ");
 		scanf("%s",&consulta);
 	}
 	mitja=suma/numero_repeticions;
